5ac1ac918704f86aea36825fe81d9e9a4d614357
DohrliofDeath <michi.dohr@gmx.at>
2021-02-14 17:24:08 +0100

    restructured project

---
+++ NotificationDaemon/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotificationDaemon
+{
+    class Program
+    {
+        static List<string> bufferedNotifications = new List<string>();
+        static void Main(string[] args)
+        {
+            string[] fileList = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
